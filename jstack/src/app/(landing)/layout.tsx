fdeab7b829eabf9f1b7338ec841ccc3004097eb9
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-26 13:48:48 +0200

    layout from (landing) navbar + children

--- jstack/src/app/(landing)/layout.tsx
+++ jstack/src/app/(landing)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "../components/navbar"                
+import { Navbar } from "@/components/navbar"                
 import { ReactNode } from "react";
 
 const Layout = ({children} : {children: ReactNode}) => {        // Ici, on crée un composant Layout qui sera utilisé pour envelopper le contenu de la page. Ce composant est une fonction fléchée qui prend en paramètre des props, ici children, qui est de type ReactNode. Cela signifie que ce composant peut recevoir n'importe quel contenu React, comme du texte, des éléments HTML, d'autres composants, etc. Par exemple, si on place du texte ou d'autres composants entre les balises d'ouverture et de fermeture du composant Layout dans le fichier page.tsx, ce contenu sera affiché ici dans le composant Layout. ({children} : {children: ReactNode}) signifie que nous attendons une prop children de type ReactNode, ce qui est courant dans les composants React pour permettre l'insertion de contenu dynamique.
