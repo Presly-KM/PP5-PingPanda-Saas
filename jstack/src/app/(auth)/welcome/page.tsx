ff982e5bcdebeb4ad7670e5ec79d2ab907751139
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-10 20:37:48 +0200

    import cmponents + yseffect + creae const page + const backgroundpattern

--- jstack/src/app/(auth)/welcome/page.tsx
+++ jstack/src/app/(auth)/welcome/page.tsx
@@ -1,13 +1,41 @@
+"use client"
 //Synchronize auth status to database
-
+import { Heading } from "@/components/heading";
+import { LoadingSpinner } from "@/components/loading-spinner";
+import { client } from "@/lib/client";
+import { useQuery } from "@tanstack/react-query";
 import { LucideProps } from "lucide-react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+    const router = useRouter()
+
+    const { data } = useQuery({
+     queryFn: async () => {
+           const res = await client.auth.getDatabaseSyncStatus.$get()
+           return await res.json()
+          },
+          queryKey: ["get-database-sync-status"],
+          refetchInterval: (query) => {
+            return query.state.data?.isSynced ? false : 1000
+          },
+        })
+      
+        useEffect(() => {
+        if (data?.isSynced) router.push("/dashboard")
+        }, [data, router])
+
     return (
     <div className="flex w-full flex-1 items-center justify-center px-4">
-        <BackgroundPattern className="abosolute inset-0 left-1/2 z-0-translate-x-1/2 opacity-75"/>
+        <BackgroundPattern className="absolute inset-0 left-1/2 z-0-translate-x-1/2 opacity-75"/>
+
     <div className="relative z-10 flex -translate-x-1/2 flex-col items-center gap-6 text-center">
-        <LoadingSpinner/>
+        <LoadingSpinner size="md"/>
+        <Heading> Creating your account...</Heading>
+        <p className="text-base/7 text-gray-600 max-w-prose">
+         Just a moment while we set things up for you.
+        </p>
       </div>
     </div>
     )
