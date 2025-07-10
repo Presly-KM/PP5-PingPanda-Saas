564e419d542c573bce8dc104a4e38b0bfe87484f
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-10 20:39:57 +0200

    procedure.ts copy past gihb josd

--- jstack/src/server/procedures.ts
+++ jstack/src/server/procedures.ts
@@ -1,11 +1,37 @@
-import { Pool } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client"
-import { Redis } from "@upstash/redis/cloudflare"
-import { env } from "hono/adapter"
-import { cacheExtension } from "./__internals/db/cache-extension"
+import { db } from "@/db"
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  const authHeader = c.req.header("Authorization")
+
+  if (authHeader) {
+    const apiKey = authHeader.split(" ")[1] // bearer <API_KEY>
+
+    const user = await db.user.findUnique({
+      where: { apiKey },
+    })
+
+    if (user) return next({ user })
+  }
+
+  const auth = await currentUser()
+
+  if (!auth) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  return next({ user })
+})
 
 /**
  * Public (unauthenticated) procedures
@@ -14,3 +40,4 @@ import { j } from "./__internals/j"
  */
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
+export const privateProcedure = publicProcedure.use(authMiddleware)
