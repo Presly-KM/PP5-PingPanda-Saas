2759e33d971d701b08d0ed8895064b9de307b977
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-12 21:46:13 +0200

    import { DashboardPageContent } from "@/app/dashboard/dashboard-page-content"

--- jstack/src/app/dashboard/page.tsx
+++ jstack/src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { DashboardPage } from "@/components/dashboard-page"
+import { DashboardPageContent } from "@/app/dashboard/dashboard-page-content"  // Importation du composant DashboardPageContent qui contient le contenu principal de la page de tableau de bord.
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
