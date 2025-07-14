026eff4f91fad8879ae95da19f689ebaa2a8357a
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-14 09:28:27 +0200

    </CreateEventCategoryModal>

--- jstack/src/app/dashboard/page.tsx
+++ jstack/src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { DashboardPageContent } from "@/app/dashboard/dashboard-page-content"  /
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { PlusIcon } from "lucide-react"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { Button } from "@/components/ui/button"                               // Le composant Button est utilisé pour
 
 const Page = async () => {
     const auth = await currentUser()
@@ -19,9 +22,21 @@ const Page = async () => {
         redirect("/sign-in")
     }
     
-    return <DashboardPage title="Dashboard">
+    return ( 
+         <DashboardPage 
+             cta={
+            <CreateEventCategoryModal> 
+                <Button> 
+                    <PlusIcon className="size-4 mr-2"/>   {/* Le composant DashboardPage est utilisé pour encapsuler le contenu de la page de tableau de bord. Il prend en charge un titre et un CTA (Call To Action) qui peut être personnalisé. Un CTA est un élément interactif qui incite l'utilisateur à effectuer une action, comme cliquer sur un bouton ou un lien. */}
+                      Add Category
+                </Button>
+            </CreateEventCategoryModal>
+            }
+                title="Dashboard"
+                >
               <DashboardPageContent/>
            </DashboardPage>
+)
 }
 
 export default Page
