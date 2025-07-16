3d631a627bf853b5db04ea5294fc3b23c417f5a8
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-16 10:18:08 +0200

    return DashboardPage

---
+++ jstack/src/app/dashboard/category/[name]/page.tsx
@@ -0,0 +1,54 @@
+import { currentUser} from "@clerk/nextjs/server"
+import { notFound } from "next/navigation"
+import { db } from "@/db"
+import { DashboardPage } from "@/components/dashboard-page"
+
+
+interface PageProps {
+  params: {
+    name: string | string[] | undefined
+  } 
+}
+
+const Page = async ({ params }: PageProps) => {
+    if (typeof params.name !== "string") return notFound()
+
+        const auth = await currentUser()
+
+        if (!auth) {
+            return notFound()
+        }
+
+        const user = await db.user.findUnique({ 
+            where: { externalId: auth.id },
+        })
+
+        if (!user) return notFound()
+        
+          const category = await db.eventCategory.findUnique({
+            where: { 
+                name_userId: {
+                    name: params.name,
+                    userId: user.id,
+                },
+            },
+            include: {
+                _count: {
+                    select: {
+                      events: true, // Inclut le nombre d'événements associés à cette catégorie.
+                    },
+                },
+            },
+        })
+
+        if (!category) return notFound()
+
+          const hasEvents = category._count.events > 0
+
+          return ( 
+            <DashboardPage title={`${category.emoji} ${category.name} events`}></DashboardPage>
+          )
+
+}
+
+export default Page
