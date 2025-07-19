6356fb11501f38ff61c05adf4ab136c0c50e1f70
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-19 23:59:39 +0200

    comment

--- jstack/src/app/dashboard/category/[name]/category-page-content.tsx
+++ jstack/src/app/dashboard/category/[name]/category-page-content.tsx
@@ -4,12 +4,12 @@ import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { EmptyCategoryState } from "./empty-category-state"
 
-interface CategoryPageContentProps {
+interface CategoryPageContentProps {           // Ici on définit les props du composant
     hasEvents: boolean
     category: EventCategory
 }
 
-export const CategoryPageContent = ({ 
+export const CategoryPageContent = ({          // Composant qui affiche le contenu de la page de catégorie
     hasEvents: initialHasEvents, 
     category,
  }: CategoryPageContentProps) => {
