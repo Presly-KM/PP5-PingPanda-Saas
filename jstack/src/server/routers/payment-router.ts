ccb0f575077592577256d81ca99c0f1c346a80de
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-28 19:48:19 +0200

    add file

---
+++ jstack/src/server/routers/payment-router.ts
@@ -0,0 +1,16 @@
+import { createCheckoutSession } from "@/lib/stripe" // Ici on importe la fonction createCheckoutSession depuis le module stripe. Cette fonction est utilisée pour créer une session de paiement avec Stripe.
+import { router } from "../__internals/router"
+import { privateProcedure } from "../procedures"
+
+export const paymentRouter = router({                // Ici on crée un routeur pour les paiements en utilisant le routeur interne de l'application.
+    createCheckoutSession: privateProcedure.mutation(async ({ c, ctx }) => { 
+            const { user } = ctx
+
+            const session = await createCheckoutSession({
+                userEmail: user.email,
+                userId: user.id,
+            })
+
+            return c.json({ url: session.url }) // Ici on retourne une réponse JSON avec une URL fictive pour la session de paiement. Cela doit être remplacé par la logique réelle de création de session de paiement.
+        }),
+})
