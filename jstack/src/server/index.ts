9f2241aeea9869646cf5e3cf89f2484a13d30aeb
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-28 19:47:08 +0200

    add routes : .route("/payment", paymentRouter)  + .route("/project", projectRouter)

--- jstack/src/server/index.ts
+++ jstack/src/server/index.ts
@@ -3,6 +3,8 @@ import { cors } from "hono/cors"
 import { handle } from "hono/vercel"
 import { authRouter } from "./routers/auth-router"
 import { categoryRouter } from "./routers/category-router"
+import { paymentRouter } from "./routers/payment-router" // Import du routeur de paiement
+import { projectRouter } from "./routers/project-router"
 
 const app = new Hono().basePath("/api").use(cors())
 
@@ -14,6 +16,8 @@ const app = new Hono().basePath("/api").use(cors())
 const appRouter = app
 .route("/auth", authRouter)
 .route("/category", categoryRouter)
+.route("/payment", paymentRouter)                       // Ajout du routeur de paiement ici
+.route("/project", projectRouter)
 
 // The handler Next.js uses to answer API requests
 export const httpHandler = handle(app)
