20b25caef054bf4b2d6deaf64021775a4b8a4e89
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-07-29 18:36:18 +0200

    SignUp into SignIn

--- jstack/src/app/(auth)/sign-up/[[...rest]]/page.tsx
+++ jstack/src/app/(auth)/sign-up/[[...rest]]/page.tsx
@@ -1,11 +1,16 @@
 "use client"            // Ici on indique que ce fichier est un composant client, ce qui signifie qu'il sera rendu côté client plutôt que côté serveur. Cela est nécessaire pour les composants qui utilisent des fonctionnalités spécifiques au client, comme les hooks React ou les interactions avec le DOM. Par e
 
-import { SignUp } from "@clerk/nextjs"  // Importation du composant SignIn de Clerk, qui gère l'interface de connexion des utilisateurs.
+import { SignIn } from "@clerk/nextjs"  // Importation du composant SignIn de Clerk, qui gère l'interface de connexion des utilisateurs.
+import { useSearchParams } from "next/navigation"
 
 const Page = () => {     // Définition du composant Page, qui sera utilisé pour afficher la page de connexion.
+   
+    const searchParams = useSearchParams() // Utilisation du hook useSearchParams de Next.js pour accéder aux paramètres de l'URL. Cela permet de récupérer des informations passées dans l'URL, comme les intentions de navigation ou les messages d'erreur.
+    const intent = searchParams.get("intent") // Extraction du paramètre "intent" de l'URL, qui peut être utilisé pour déterminer l'action à effectuer après la connexion.
+   
     return ( 
        <div className="w-full flex-1 flex items-center justify-center">
-           <SignUp />   {/* Utilisation du composant SignIn de Clerk pour afficher le formulaire de connexion. use client permet d'utiliser les fonctionnalités de Clerk côté client, comme la gestion des sessions et l'authentification. */}
+           <SignIn />   {/* Utilisation du composant SignIn de Clerk pour afficher le formulaire de connexion. use client permet d'utiliser les fonctionnalités de Clerk côté client, comme la gestion des sessions et l'authentification. */}
         </div>
     )
 }
